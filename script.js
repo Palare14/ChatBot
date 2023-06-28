a3d40912d4e54d37e0af6aa2a6808c892c49c216
Palare14 <137983361+Palare14@users.noreply.github.com>
2023-06-28 09:25:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,69 +6,70 @@ const chatLog = document.querySelector('.chat-log');
 // Array med kunnskapsbaserte spørsmål og svar
 const sporsmalOgSvar = [
   {
-    sporsmal: 'Hva er verdens største dyr',
-    svar: 'Det største dyret på jorden er blåhvalen. Den kan bli lengre enn et fotballbane!',
-  },
-  {
-    sporsmal: 'Hvorfor er himmelen blå',
-    svar: 'Himmelen ser blå ut fordi lysstråler fra solen spres av partikler i atmosfæren vår.',
-  },
-  {
-    sporsmal: 'Hvordan lager bier honning',
-    svar: 'Bier lager honning ved å samle nektar fra blomster og deretter fordøye det og lagre det i honningkakene sine.',
-  },
-  {
-    sporsmal: 'Hva er det lengste elven i verden',
-    svar: 'Den lengste elven i verden er Nilen i Afrika. Den er over 6 600 kilometer lang!',
-  },
-  {
-    sporsmal: 'Hvorfor blir bladene på trærne grønne om sommeren og faller av om vinteren',
-    svar: 'Om sommeren produserer trærne klorofyll, som gir bladene den grønne fargen. Om vinteren går trærne i dvale, og bladene faller av.',
-  },
-  {
-    sporsmal: 'Hva er en dinosaur',
-    svar: 'Dinosaurer var utdødde krypdyr som levde for mange millioner år siden. De kom i mange forskjellige størrelser og former.',
-  },
-  {
-    sporsmal: 'Hvorfor trenger vi å sove',
-    svar: 'Vi trenger å sove fordi kroppen vår trenger hvile og tid til å reparere seg selv. Søvn hjelper oss å føle oss friske og våkne.',
-  },
-  {
-    sporsmal: 'Hvordan lager man sjokolade',
-    svar: 'Sjokolade lages ved å blande kakaobønner med sukker og andre ingredienser. Deretter blir blandingen bearbeidet og formet til sjokolade.',
-  },
-  {
-    sporsmal: 'Hvor mange planeter er det i solsystemet vår',
-    svar: 'Det er åtte planeter i solsystemet vårt. De er Merkur, Venus, Jorden, Mars, Jupiter, Saturn, Uranus og Neptun.',
-  },
-  {
-    sporsmal: 'Hvorfor får man hikke',
-    svar: 'Man får hikke når mellomgulvet, en muskel under lungene våre, trekker seg sammen brått. Dette får stemmebåndene til å lukke seg, og det lager lyden vi kaller hikke.',
-  },
-  {
-    sporsmal: 'Kan jeg få is',
-    svar: 'Nei',
-  },
-  {
-    sporsmal: 'Er det langt igjen',
-    svar: 'Ja',
-  },
-  {
-    sporsmal: 'Er vi fremme snart',
-    svar: 'Nei',
-  },
-  {
-    sporsmal: 'Hva er verdens største dyreart',
-    svar: 'Den største dyrearten på jorden er blåhvalen.',
-  },
-  {
-    sporsmal: 'Hva er verdens høyeste fjell',
-    svar: 'Det høyeste fjellet i verden er Mount Everest.',
-  },
-  {
-    sporsmal: 'Hva er det raskeste dyret på land',
-    svar: 'Det raskeste dyret på land er geparden.',
-  },
+  sporsmal: 'Hva er verdens største dyr',
+  svar: 'Det største dyret på jorden er blåhvalen. ',
+  funfact: 'Blåhvalen kan bli lengre enn et fotballbane! Hjertet alene kan være så stort som en bil!'
+},
+{
+  sporsmal: 'Hvorfor er himmelen blå',
+  svar: 'Himmelen ser blå ut fordi lysstråler fra solen spres av partikler i atmosfæren vår.',
+},
+{
+  sporsmal: 'Hvordan lager bier honning',
+  svar: 'Bier lager honning ved å samle nektar fra blomster og deretter fordøye det og lagre det i honningkakene sine.',
+},
+{
+  sporsmal: 'Hva er det lengste elven i verden',
+  svar: 'Den lengste elven i verden er Nilen i Afrika. Den er over 6 600 kilometer lang!',
+},
+{
+  sporsmal: 'Hvorfor blir bladene på trærne grønne om sommeren og faller av om vinteren',
+  svar: 'Om sommeren produserer trærne klorofyll, som gir bladene den grønne fargen. Om vinteren går trærne i dvale, og bladene faller av.',
+},
+{
+  sporsmal: 'Hva er en dinosaur',
+  svar: 'Dinosaurer var utdødde krypdyr som levde for mange millioner år siden. De kom i mange forskjellige størrelser og former.',
+},
+{
+  sporsmal: 'Hvorfor trenger vi å sove',
+  svar: 'Vi trenger å sove fordi kroppen vår trenger hvile og tid til å reparere seg selv. Søvn hjelper oss å føle oss friske og våkne.',
+},
+{
+  sporsmal: 'Hvordan lager man sjokolade',
+  svar: 'Sjokolade lages ved å blande kakaobønner med sukker og andre ingredienser. Deretter blir blandingen bearbeidet og formet til sjokolade.',
+},
+{
+  sporsmal: 'Hvor mange planeter er det i solsystemet vår',
+  svar: 'Det er åtte planeter i solsystemet vårt. De er Merkur, Venus, Jorden, Mars, Jupiter, Saturn, Uranus og Neptun.',
+},
+{
+  sporsmal: 'Hvorfor får man hikke',
+  svar: 'Man får hikke når mellomgulvet, en muskel under lungene våre, trekker seg sammen brått. Dette får stemmebåndene til å lukke seg, og det lager lyden vi kaller hikke.',
+},
+{
+  sporsmal: 'Kan jeg få is',
+  svar: 'Nei',
+},
+{
+  sporsmal: 'Er det langt igjen',
+  svar: 'Ja',
+},
+{
+  sporsmal: 'Er vi fremme snart',
+  svar: 'Nei',
+},
+{
+  sporsmal: 'Hva er verdens største dyreart',
+  svar: 'Den største dyrearten på jorden er blåhvalen.',
+},
+{
+  sporsmal: 'Hva er verdens høyeste fjell',
+  svar: 'Det høyeste fjellet i verden er Mount Everest.',
+},
+{
+  sporsmal: 'Hva er det raskeste dyret på land',
+  svar: 'Det raskeste dyret på land er geparden.',
+},
 ];
 
 // Lytter til klikk på send-knappen
@@ -111,15 +112,16 @@ function visSporsmal(sporsmal) {
 // Funksjon for å generere svar basert på spørsmålet
 function genererSvar(sporsmal) {
   // Finn svar basert på spørsmålet
-  const svar = finnSvar(sporsmal);
+  const svarData = finnSvar(sporsmal);
 
-  // Vis animasjon når chatboten tenker
-  visTenkerAnimasjon();
+  // Viser animasjon når chatboten tenker
+  chatLog.classList.add('tenker');
 
   // Simulerer en forsinkelse før svaret vises
   setTimeout(function () {
     // Viser svaret i chatloggen
-    visSvar(sporsmal, svar);
+    visSvar(sporsmal, svarData.svar, svarData.funfact);
+    chatLog.classList.remove('tenker');
   }, 1000);
 }
 
@@ -128,20 +130,14 @@ function finnSvar(sporsmal) {
   for (let i = 0; i < sporsmalOgSvar.length; i++) {
     const spmOgSvar = sporsmalOgSvar[i];
     if (sporsmal.toLowerCase().includes(spmOgSvar.sporsmal.toLowerCase())) {
-      return spmOgSvar.svar;
+      return spmOgSvar;
     }
   }
-  return 'Beklager, jeg vet ikke svaret på det spørsmålet.';
+  return { svar: 'Beklager, jeg vet ikke svaret på det spørsmålet.' };
 }
 
 // Funksjon for å vise svaret i chatloggen
-function visSvar(sporsmal, svar) {
-  // Fjerner tenker-animasjonen
-  const tenkerElement = document.querySelector('.tenker');
-  if (tenkerElement) {
-    tenkerElement.remove();
-  }
-
+function visSvar(sporsmal, svar, funfact) {
   // Oppretter et element for spørsmålet
   const sporsmalElement = document.createElement('div');
   sporsmalElement.classList.add('sporsmal');
@@ -154,20 +150,13 @@ function visSvar(sporsmal, svar) {
   svarElement.innerHTML = `<strong>Svar:</strong><br>${svar}`;
   chatLog.appendChild(svarElement);
 
-  // Oppretter et element for mellomrom
-  const mellomromElement = document.createElement('br');
-  chatLog.appendChild(mellomromElement);
-
-  // Ruller chatloggen automatisk til bunnen
-  chatLog.scrollTop = chatLog.scrollHeight;
-}
-
-// Funksjon for å vise animasjon når chatboten tenker
-function visTenkerAnimasjon() {
-  const tenkerElement = document.createElement('div');
-  tenkerElement.classList.add('tenker');
-  tenkerElement.innerHTML = '<span></span><span></span><span></span>';
-  chatLog.appendChild(tenkerElement);
+  // Oppretter et element for funfacten
+  if (funfact) {
+    const funfactElement = document.createElement('div');
+    funfactElement.classList.add('funfact');
+    funfactElement.innerHTML = `<strong>Fun Fact:</strong><br>${funfact}`;
+    chatLog.appendChild(funfactElement);
+  }
 
   // Oppretter et element for mellomrom
   const mellomromElement = document.createElement('br');
